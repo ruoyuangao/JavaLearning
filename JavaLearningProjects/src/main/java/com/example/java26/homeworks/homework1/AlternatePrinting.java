9903ae3f81bbb28295639ecb4034dcb49a38b15e
Kelly(Ruoyuan) Gao <g.ruoyuan@wustl.edu>
2023-01-15 19:45:47 -0600

    initial homework2

---
+++ JavaLearningProjects/src/main/java/com/example/java26/homeworks/homework1/AlternatePrinting.java
@@ -0,0 +1,50 @@
+package com.example.java26.homeworks.homework1;
+
+import java.util.concurrent.locks.ReentrantLock;
+public class AlternatePrinting {
+
+    private static final ReentrantLock lock = new ReentrantLock();
+
+    private static int num = 1;
+
+    private static char c = 'a';
+    private static volatile boolean flag = true;
+
+    public static void main(String[] args) throws InterruptedException {
+        Thread t1 = new Thread(()->{
+            while (num <= 26) {
+                if (flag) {
+                    try {
+                        lock.lock();
+                        System.out.println(Thread.currentThread().getName()+":"+num);
+                        ++num;
+                        flag = false;
+                    } finally {
+                        lock.unlock();
+                    }
+
+                }
+            }
+        });
+
+        Thread t2 = new Thread(()->{
+            while (c <= 'z') {
+                if (!flag) {
+                    try {
+                        lock.lock();
+                        System.out.println(Thread.currentThread().getName()+":"+c);
+                        ++c;
+                        flag = true;
+                    } finally {
+                        lock.unlock();
+                    }
+                }
+            }
+        });
+
+        t1.start();
+        t2.start();
+        t1.join();
+        t2.join();
+    }
+}
